470249fd73afee7d1d88f973f0e994eb92802eef
Savita Datta <savitadatta98@gmail.com>
2024-12-16 10:34:24 +1000

    links open in new tab when clicked; link search includes href

--- content-scripts/content-script.js
+++ content-scripts/content-script.js
@@ -1,15 +1,20 @@
 chrome.runtime.onMessage.addListener((msg) => {
     if (msg.text === "getHelp") {
         var links = document.getElementsByTagName("a");
-        for (var i = 0; i < links.length; i++) {
-            var btn = links[i];
-            var name = btn.textContent || btn.innerText;
+        var sent = [];
 
-            if (name.toLowerCase().includes("about") || name.toLowerCase().includes("help")) {
-                chrome.runtime.sendMessage({
-                    from: "getHelp",
-                    text: links[i].href
-                });
+        for (let link of links) {
+            var name = link.textContent || link.innerText;
+
+            if (name.toLowerCase().includes("about") || name.toLowerCase().includes("help")
+                    || link.href.includes("about") || link.href.includes("help")) {
+                if (!sent.includes(link.href)) {
+                    chrome.runtime.sendMessage({
+                        from: "getHelp",
+                        text: link.href
+                    });
+                    sent.push(link.href);
+                }
             }
         }   
     }
--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,5 @@
 document.querySelector("#getHelp")
     .addEventListener('click', () => {
-        chrome.runtime.sendMessage({
-            text: "Popup"
-        });
-
         chrome.tabs.query({
             active: true,
             currentWindow: true
@@ -18,6 +14,6 @@ document.querySelector("#getHelp")
 chrome.runtime.onMessage.addListener((msg) => {
     if (msg.from === "getHelp") {
         document.getElementById("results").innerHTML
-            += `<a class="button" href="${msg.text}">${msg.text}</a>`;
+            += `<a class="button" href="${msg.text}" target="_blank">${msg.text}</a>`;
     }
 });
